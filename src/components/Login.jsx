59c534fae6bb05cb444fddeca9c0d5b04297187a
Jeff Patten <jeffspatten@gmail.com>
2024-08-15 16:11:07 -0700

    added login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Login = () => {
+    //blurred version of kitchen showing a login. Used to access users events and cookbooks.
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
