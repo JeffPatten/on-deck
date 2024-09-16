2913e89e0ccf938fddb0510bbc8bd3b1be362d68
Jeff Patten <jeffspatten@gmail.com>
2024-09-16 14:52:46 -0700

    add logic to recipe component

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Ingredient from './Ingredient/Ingredient'
 
-const Recipe = () => {
+const Recipe = (props) => {
     // Will contain the actual recipe. Also will show number of servings, to be used in ensuring enough food for number of people at an event. Ingredients component will be a child component on the recipe. Recipes will be added to the cook books.
 
+    const {recipe} = props
+
   return (
-    <div>Recipe</div>
+    <div>
+      <h2>{recipe.name}</h2>
+      {recipe.ingredients.map((ingredient, index) => {
+        return <Ingredient name={ingredient.name} amount={ingredient.amount} key={index} />
+      })}
+      <p>{recipe.instructions}</p>
+    </div>
   )
 }
 
