92e0f5d6359c2314ab8b5ace3b26e6a7fafc45e2
Jeff Patten <jeffspatten@gmail.com>
2024-09-19 14:55:31 -0700

    update book modal to use recipe component

--- src/components/BookModal/BookModal.jsx
+++ src/components/BookModal/BookModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Ingredient from "../Ingredient/Ingredient";
+import Recipe from "../Recipe";
 
 const BookModal = (props) => {
   const { isOpen, onClose, recipes } = props;
@@ -12,13 +13,14 @@ const BookModal = (props) => {
   const displayedRecipes = recipes.map((recipe)=>{
     return (
       <div>
-        <h2>{recipe.name}</h2>
+        {/* <h2>{recipe.name}</h2>
         <ul>
           {recipe.ingredients.map((ingredient,index)=> {
             return <h4>{ingredient.name}                      {ingredient.amount}</h4>
           })}
         </ul>
-        <p>{recipe.instructions}</p>
+        <p>{recipe.instructions}</p> */}
+        <Recipe recipe={recipe} />
       </div>
     )
     })
