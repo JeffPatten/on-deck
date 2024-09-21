87b06ddf0cfe4699f079b68e138971d4d1f7afda
Jeff Patten <jeffspatten@gmail.com>
2024-09-20 17:35:46 -0700

    update code for menu and course components

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Course = () => {
+const Course = (props) => {
     //Organizes the menu items by course if a user wants to plan multiple courses.
+    const {courseInfo} = props;
+
   return (
-    <div>Course</div>
+    <div>
+      {courseInfo.map((foodItem) => {
+        return (
+          <>
+            <h2>{foodItem.title}</h2>
+            <p>{foodItem.desciption}</p>
+            <h4>{foodItem.servings}</h4>
+            <h4>{foodItem?.restrictions}</h4>
+          </>
+        )
+      })}
+    </div>
   )
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
+import Course from "./Course";
 
-const Menu = () => {
-    //This is where courses will be added for an event. Menu will be displayed on the event component. Courses will be displayed here. Stretch goal: create a quick event to add in the kitchen component for everyday meals (instead of an actual event with guests).
+const Menu = (props) => {
+  //This is where courses will be added for an event. Menu will be displayed on the event component. Courses will be displayed here. Stretch goal: create a quick event to add in the kitchen component for everyday meals (instead of an actual event with guests).
+  const { courses } = props;
+
   return (
-    <div>Menu</div>
-  )
-}
+    <div>
+      {courses.map((course, index) => {
+        return (
+          <Course courseInfo={course} key={index}/>
+        )
+      })
+      }
+      </div>
+  );
+};
 
-export default Menu
+export default Menu;
